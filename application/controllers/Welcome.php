b5fc762847d7ade93c56226dd82776fa0f6ede38
renfridfrancis@gmail.com <renfridfrancis@gmail.com>
2016-04-08 15:41:39 +0300

    feature admin feature

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->data['feature_admin'] = "Feature Admin";
 		$this->data['title'] = "Sample Projects";
 		$this->load->view('welcome_message');
 	}
